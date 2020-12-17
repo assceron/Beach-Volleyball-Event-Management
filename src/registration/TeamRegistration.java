72e4148ae10b40af28d461c4a02679ad06855df6
Assunta Cerone <assuntacerone@gmail.com>
2020-12-18 00:26:25 +0800

    images

--- src/registration/TeamRegistration.java
+++ src/registration/TeamRegistration.java
@@ -1,14 +1,37 @@
 package registration;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 import databases.PlayersDB;
 import databases.TeamsDB;
+import management.MessageEvent;
 
 public class TeamRegistration {
 	private TeamsDB teamsDB = new TeamsDB();
 	private PlayersDB playersDB = new PlayersDB();
 
+	private long dateDifference(String eventDate) {
+		long diff = -1;
+		try {
+		    LocalDate now = LocalDate.now();
+		    DateTimeFormatter formatter = new DateTimeFormatterBuilder().parseCaseInsensitive()
+		            .append(DateTimeFormatter.ofPattern("dd/MMM/yyyy")).toFormatter();
+		 
+		    LocalDate date2 = LocalDate.parse(eventDate, formatter);
+		    diff = now.until(date2, ChronoUnit.DAYS);
+		    
+		} catch (DateTimeParseException e) {
+	         // Exception handling message/mechanism/logging as per company standard
+			System.out.println(e.getMessage());
+		}
+		return diff;
+	}
+	
 	private int registerNewPlayer(Player p) {
 		playersDB.createPlayersDB(); 
 		
@@ -21,16 +44,25 @@ public class TeamRegistration {
 		return playerID;
 	}
 	
-	public int registerNewTeam(String teamName, String gameNight, ArrayList<Player> players) {
+	public int registerNewTeam(String teamName, String dateOfGame, ArrayList<Player> players) {
 		teamsDB.createTeamsDB(); //TO MOVE
 		int teamID = -1;
-
+		
+		/*
+		 * When register a new team, check for dateOfGame. The registration closes 7 days before the game.
+		 * 		long diff = dateDifference(dateOfGame);
+		 * 			if(diff < 7 ) {
+		 * 				System.out.println("Registration are closed");
+		 * 				return teamID;
+		 * 			}
+		 * */
+		
 		if(!this.teamExistent(teamName)) {
     		/*TEAM NOT EXISTENT*/	
 			for(Player p:players) 
 				p.setPlayerID(registerNewPlayer(p));	
 			
-	    	teamID = teamsDB.insertTeam(new Team(teamName,players,gameNight));			
+	    	teamID = teamsDB.insertTeam(new Team(teamName,players,dateOfGame));			
 		}
     	return teamID;
 	}	
